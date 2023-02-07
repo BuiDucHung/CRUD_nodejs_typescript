1e9c7a57c42cadc3bdfc18ca6c9f92d0b6823184
hung <anhkhongbietroi@gmail.com>
2023-02-07 17:38:36 +0700

    CRUD nodejs

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+/**
+ * Required External Modules
+ */
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { itemRouter } from "./items/items.router";
+ 
+ dotenv.config();
+/**
+ * App Variables
+ */
+ const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
+
+ const app = express();
+/**
+ *  App Configuration
+ */
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use("/api/menu/items", itemRouter);
+/**
+ * Server Activation
+ */
+
+
+
+app.listen(PORT, () => {
+console.log(`Express is listening at http://localhost:${PORT}`);
+});
